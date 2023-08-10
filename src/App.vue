ea5299126579487c42c500a40f07866e77abfbfe
bhavik712 <baradbhavik41@gmail.com>
2023-08-10 19:50:31 +0530

    Component Created

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <form-input :fieldProperties="inputData"></form-input>
+</template>
+
+<script>
+import FormInput from "./components/FormInput.vue";
+export default {
+  name: "app",
+  data() {
+    return {
+      inputData: {
+        label: "Name",
+        name: "userName",
+        type: "text",
+        placeholder: "Enter Your Name",
+      },
+    };
+  },
+  components: {
+    FormInput,
+  },
+};
+</script>
---
+++ src/components/FormInput.vue
@@ -0,0 +1,79 @@
+<template>
+  <label class="input-label">{{ fieldLabel }}</label>
+  <input
+    class="input-field"
+    :name="fieldName"
+    :type="fieldtype"
+    :placeholder="fieldPlaceholder"
+  />
+</template>
+
+<script>
+export default {
+  name: "FormInput",
+  props: {
+    fieldProperties: {
+      type: Object,
+      validator: (propertyObj) => {
+        if (
+          !propertyObj.hasOwnProperty("label") ||
+          !propertyObj.hasOwnProperty("name") ||
+          !propertyObj.hasOwnProperty("type") ||
+          !propertyObj.hasOwnProperty("placeholder")
+        ) {
+          return false;
+        } else if (
+          typeof propertyObj.label !== "string" ||
+          typeof propertyObj.name !== "string" ||
+          typeof propertyObj.type !== "string" ||
+          typeof propertyObj.placeholder !== "string"
+        ) {
+          return false;
+        } else if (
+          !propertyObj.label.length ||
+          !propertyObj.name.length ||
+          !propertyObj.type.length ||
+          !propertyObj.placeholder.length
+        ) {
+          return false;
+        } else {
+          return true;
+        }
+      },
+    },
+  },
+  computed: {
+    fieldLabel() {
+      return this.fieldProperties.label;
+    },
+    fieldName() {
+      return this.fieldProperties.name;
+    },
+    fieldtype() {
+      return this.fieldProperties.type;
+    },
+    fieldPlaceholder() {
+      return this.fieldProperties.placeholder;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-label {
+  display: inline-block;
+  margin-bottom: 10px;
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.input-field {
+  font-size: 25px;
+  display: block;
+  width: 300px;
+  color: gray;
+  border: 2px solid black;
+  border-radius: 10px;
+  padding: 5px;
+}
+</style>
